d2c1308854724beb8add190f59b7bc8d2709469d
ignazioi <ignazio.ingenito@ocsnet.it>
2024-06-23 20:30:35 +0200

    chore(settings): clean up

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,18 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
+  compressHTML: true,
+  experimental: {
+    contentCollectionCache: true
+  },
+  integrations: [tailwind()],
+  markdown: {
+    shikiConfig: {
+      theme: "none"
+    }
+  },
   site: 'https://www.baialupo.com',
-  integrations: [tailwind()]
+  trailingSlash: "never"
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,33 +6,15 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		screens: {
-			"sm": "640px",
-			"md": "768px",
-			"lg": "1024px",
-			"xl": "1280px",
-			"2xl": "1536px",
-		},
 		extend: {
 			fontSize: {
-				"xs": ".75rem",
-				"sm": ".875rem",
-				"tiny": ".875rem",
 				"base": "0.975rem",
-				"lg": "1.125rem",
-				"xl": "1.25rem",
-				"2xl": "1.5rem",
-				"3xl": "1.875rem",
-				"4xl": "2.25rem",
-				"5xl": "3rem",
-				"6xl": "4rem",
-				"7xl": "5rem",
 			},
-		},
-		fontFamily: {
-			"sans": ["'Encode Sans'", "system-ui", "ui-sans-serif", "sans-serif"],
-			"serif": ["'Encode Sans'", "ui-serif", "serif"],
-			"mono": ["Incosolata", "ui-monospace", "monospace"],
+			fontFamily: {
+				"sans": ["'Encode Sans'", "system-ui", "ui-sans-serif", "sans-serif"],
+				"serif": ["'Encode Sans'", "ui-serif", "serif"],
+				"mono": ["Incosolata", "ui-monospace", "monospace"],
+			},
 		},
 	},
 	plugins: [],

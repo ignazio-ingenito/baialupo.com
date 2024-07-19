b920a8a825802fe091da1f3871184be84eaf792a
ignazio-ingenito <ignazio@baialupo.com>
2024-07-19 08:38:00 +0200

    fix(css): mobile menu fixed

--- src/consts.ts
+++ src/consts.ts
@@ -9,13 +9,14 @@ export const AIRPORT_AW_CODE: string = "AW-317499"
 interface MenuItem {
     label: string
     path: string
+    icon: string
 }
 
 export const MENU_ITEMS: MenuItem[] = [
-    { label: 'Aviosuperficie', path: '/aviosuperficie' },
-    { label: 'Blog', path: '/posts' },
-    { label: 'Galleria', path: '/gallery' },
-    { label: 'Mercatino', path: '/market' },
-    { label: 'Meteo', path: '/meteo' },
-    { label: 'ScuolaVolo', path: 'http://flying.baialupo.com' },
+    { label: 'Aviosuperficie', path: '/aviosuperficie', icon: "airplane" },
+    { label: 'Blog', path: '/posts', icon: "blog" },
+    { label: 'Galleria', path: '/gallery', icon: "gallery" },
+    { label: 'Mercatino', path: '/market', icon: "market" },
+    { label: 'Meteo', path: '/meteo', icon: "meteo" },
+    { label: 'ScuolaVolo', path: 'http://flying.baialupo.com', icon: "school" },
 ]
